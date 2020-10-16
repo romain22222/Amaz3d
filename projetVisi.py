f721122b270c4dfda2cefc1711bbfa48892c5f05
louisebollard <72506187+louisebollard@users.noreply.github.com>
2020-10-16 15:07:58 +0200

    Update projetVisi.py

--- projetVisi.py
+++ projetVisi.py
@@ -1,5 +1,5 @@
 import sys, time, pygame
-
+from joueur import *
 # https://fr.wikibooks.org/wiki/Pygame/Version_imprimable
 # https://www.pygame.org/docs/ref/key.html#pygame.key.name
 # https://github.com/TheAwesomePossum/StartPy/blob/3d6c824259fdf4e9e78e3138d2c9c0fecedf645f/Events.py
@@ -9,7 +9,7 @@ pygame.init()
 black = 0, 0, 0
 
 
-size = width, height = 1200, 600
+size = width, height = 1200, 800
 
 
 screen = pygame.display.set_mode(size)
@@ -19,7 +19,7 @@ perso = pygame.transform.rotozoom(perso, 0, 0.25)
 ''' on divise la taille du personnage par 4 et on le tourne de 0 degre'''
 persovar = perso.get_rect()         
 persovar = persovar.move([600,500])
-
+pygame.key.set_repeat(60, 60)
 
 
 while 1:
@@ -27,8 +27,10 @@ while 1:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            sys.exit()
-
+            pygame.quit()
+            quit()
+        persovar=movejoueur(persovar)
+        
 
     screen.fill(black)
 
@@ -37,4 +39,3 @@ while 1:
     time.sleep(0.01)
 
     pygame.display.flip()
-    print(pygame.key.name())
