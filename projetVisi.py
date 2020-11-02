68075735134a61c62d37c2f0f3ca2959235766e5
Louise Bollard <louiisebld@gmail.com>
2020-11-02 14:28:16 +0100

    Update projetVisi.py

--- projetVisi.py
+++ projetVisi.py
@@ -1,16 +1,15 @@
 import sys, time, pygame
 from joueur import *
+from constantes import *
+
 # https://fr.wikibooks.org/wiki/Pygame/Version_imprimable
 # https://www.pygame.org/docs/ref/key.html#pygame.key.name
 # https://github.com/TheAwesomePossum/StartPy/blob/3d6c824259fdf4e9e78e3138d2c9c0fecedf645f/Events.py
 # https://www.pygame.org/docs/ref/key.html#pygame.key.get_pressed
+
 pygame.init()
 
 black = 0, 0, 0
-
-
-size = width, height = 1200, 800
-
 
 screen = pygame.display.set_mode(size)
 
