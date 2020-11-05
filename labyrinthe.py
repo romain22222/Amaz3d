6076f400cfcfbcf72169bd5beff6cc7732136206
romain22222 <romrom22222@hotmail.fr>
2020-11-05 15:19:00 +0100

    Update labyrinthe.py

--- labyrinthe.py
+++ labyrinthe.py
@@ -2,18 +2,6 @@ import pygame
 import random
 
 from constantes import *
-from projetVisi import screen
-pygame.init()
-
-done = False
-
-cols = rows = 12
-
-widthPcnt = 0.3
-heightPcnt = 0.8
-wr = widthPcnt*width/cols
-hr = heightPcnt*height/rows
-
 class Spot:
     def __init__(self, x, y):
         self.x = x
@@ -48,61 +36,60 @@ class Spot:
             self.neighbors.append(grid[self.x + 1][self.y])
         if self.y < cols - 1:
             self.neighbors.append(grid[self.x][self.y + 1])
+def createLaby(cols = 6, rows = 6, widthPcnt = 0.3, heightPcnt = 0.8, gridInitPosCol= int(random.random()*cols), gridInitPosRow= int(random.random()*rows))
+    pygame.init()
+    done = False
+    wr = widthPcnt*width/cols
+    hr = heightPcnt*height/rows
+    grid = [[Spot(i, j) for j in range(cols)] for i in range(rows)]
+
+    for i in range(rows):
+        for j in range(cols):
+            grid[i][j].add_neighbors()
+
+    current = grid[gridInitPosCol][gridInitPosRow]
+    visited = [current]
+    completed = False
 
 
-grid = [[Spot(i, j) for j in range(cols)] for i in range(rows)]
-
-for i in range(rows):
-    for j in range(cols):
-        grid[i][j].add_neighbors()
-
-gridInitCol = int(random.random()*cols)
-gridInitRow = int(random.random()*rows)
-current = grid[gridInitCol][gridInitRow]
-visited = [current]
-completed = False
-
-
-def breakwalls(a, b):
-    if a.y == b.y and a.x > b.x:
-        grid[b.x][b.y].walls[1] = False
-        grid[a.x][a.y].walls[3] = False
-    if a.y == b.y and a.x < b.x:
-        grid[a.x][a.y].walls[1] = False
-        grid[b.x][b.y].walls[3] = False
-    if a.x == b.x and a.y < b.y:
-        grid[b.x][b.y].walls[0] = False
-        grid[a.x][a.y].walls[2] = False
-    if a.x == b.x and a.y > b.y:
-        grid[a.x][a.y].walls[0] = False
-        grid[b.x][b.y].walls[2] = False
-
-
-screen.fill(BLACK)
-if not completed:
-    grid[current.x][current.y].visited = True
-    got_new = False
-    temp = 10
-
-    while not got_new and not completed:
-        r = random.randint(0, len(current.neighbors)-1)
-        Tempcurrent = current.neighbors[r]
-        if not Tempcurrent.visited:
-            visited.append(current)
-            current = Tempcurrent
-            got_new = True
-        if temp == 0:
-            temp = 10
-            if len(visited) == 0:
-                completed = True
-                break
-            else:
-                current = visited.pop()
-        temp = temp - 1
+    def breakwalls(a, b):
+        if a.y == b.y and a.x > b.x:
+            grid[b.x][b.y].walls[1] = False
+            grid[a.x][a.y].walls[3] = False
+        if a.y == b.y and a.x < b.x:
+            grid[a.x][a.y].walls[1] = False
+            grid[b.x][b.y].walls[3] = False
+        if a.x == b.x and a.y < b.y:
+            grid[b.x][b.y].walls[0] = False
+            grid[a.x][a.y].walls[2] = False
+        if a.x == b.x and a.y > b.y:
+            grid[a.x][a.y].walls[0] = False
+            grid[b.x][b.y].walls[2] = False
 
     if not completed:
-        breakwalls(current, visited[len(visited)-1])
+        grid[current.x][current.y].visited = True
+        got_new = False
+        temp = 10
 
+        while not got_new and not completed:
+            r = random.randint(0, len(current.neighbors)-1)
+            Tempcurrent = current.neighbors[r]
+            if not Tempcurrent.visited:
+                visited.append(current)
+                current = Tempcurrent
+                got_new = True
+            if temp == 0:
+                temp = 10
+                if len(visited) == 0:
+                    completed = True
+                    break
+                else:
+                    current = visited.pop()
+            temp = temp - 1
+
+        if not completed:
+            breakwalls(current, visited[len(visited)-1])
+def printLaby():
     for i in range(rows):
         for j in range(cols):
             grid[i][j].show(WHITE)
