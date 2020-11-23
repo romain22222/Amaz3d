59caf82f3e7771af36d023bf5bcfb08476c7657a
jducrey <72504906+jducrey@users.noreply.github.com>
2020-11-23 09:44:44 +0100

    Minion et class Joueur

--- joueur.py
+++ joueur.py
@@ -1,20 +1,29 @@
 import sys, time, pygame
-from constantes import * 
+from constantes import *
+from labyrinthe import *
 
-def movejoueur(persovar):
-    keys = pygame.key.get_pressed()
+
+class Joueur:
+    def __init__(self,x,y,grid):
+        self.onCase=grid[x][y]
+        self.inventaire=[]
+        self.sprite=pygame.image.load("Minion.png")
     
-    if keys[pygame.K_UP] and persovar[1]-10>0:
-        bouger=persovar.move([0,-10])
-    elif keys[pygame.K_DOWN] and persovar[1]+persovar[3]+10<height:
-        bouger=persovar.move([0,10])
-    elif keys[pygame.K_RIGHT] and persovar[0]+persovar[2]+10<width:
-        bouger=persovar.move([10,0])
-    elif keys[pygame.K_LEFT] and persovar[0]-10>0:
-        bouger=persovar.move([-10,0])
-    else:
-        bouger=persovar.move([0,0])
-    return bouger
+    def movejoueur(persovar):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_UP] and not self.onCase.walls[0]:
+            self.onCase=grid[x][y-1]
+        elif keys[pygame.K_DOWN] and not self.onCase.walls[2]:
+            self.onCase=grid[x][y+1]
+        elif keys[pygame.K_RIGHT] and not self.onCase.walls[1]:
+            self.onCase=grid[x+1][y]
+        elif keys[pygame.K_LEFT] and not self.onCase.walls[3]:
+            self.onCase=grid[x-1][y]
+        else:
+            self.onCase=grid[x][y]
+
+    
 
 
 
